c3b2afe61311a6b0fe57f1fd8489744d0e026504
LakshmiPrasannamekala <lakshmiprasannamekala0@gmail.com>
2022-07-18 22:19:30 +0530

    add.java

---
+++ add.java
@@ -0,0 +1,7 @@
+public class SumOfNumbers4.
+{
+public static void main(String args[])
+{
+int x = Integer.parseInt(args[0]); //first arguments.
+int y = Integer.parseInt(args[1]); //second arguments.
+int sum = x + y;
